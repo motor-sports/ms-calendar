427e1a664727131e645698c578cf4c9090b0cb68
motor-sports <mjkovadev@gmail.com>
2025-04-03 16:14:14 -0400

    fixing fetch

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 console.log("running")
 
-fetch("https://api.wrc.com/content/filters/calendar?championship=wrc&origin=vcms&year=2025")
+fetch('https://api.wrc.com/content/filters/calendar?championship=wrc&origin=vcms&year=2025', { method: "GET", mode: 'cors', headers: { 'Content-Type': 'application/json',}})
 .then(response => response.json())
 .then(data => {
     data.forEach((data) => {
