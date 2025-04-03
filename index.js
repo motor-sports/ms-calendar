b1c85846c873f76a76c0b83fadbe009e357f0971
motor-sports <mjkovadev@gmail.com>
2025-04-03 16:05:35 -0400

    Testing JS Functionality

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-fetch('https://api.wrc.com/content/filters/calendar?championship=wrc&origin=vcms&year=2025')
+console.log("running")
+
+fetch("https://api.wrc.com/content/filters/calendar?championship=wrc&origin=vcms&year=2025")
 .then(response => response.json())
 .then(data => {
     data.forEach((data) => {
